d02f374e26f5e680f72ec1a69c8ae3493973b878
mendo-sc <mendo@student.cscc.edu>
2025-05-10 15:00:26 -0400

    Reorganized URLs and dictionary append outside of functions

--- hn_submissions.py
+++ hn_submissions.py
@@ -2,43 +2,40 @@ from operator import itemgetter
 
 import requests
 
-def check_status():
+def check_status(url):
     """Make an API call and check the response.
 
     Returns:
         Response: Server data from url
     """
-    url = "https://hacker-news.firebaseio.com/v0/topstories.json"
     r = requests.get(url)
     print(f"Status code: {r.status_code}")
     return r
 
-def make_api_call(submission_id):
+def make_api_call(url, submission_id):
     """Make a new API call for each submission
 
     Args:
         submission_id (_type_): _description_
     """
-    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
     r = requests.get(url)
     print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
     return response_dict
     
-def build_article_dict(submission_dicts, submission_id, response_dict):
-    """Build a dictionary for each article
+def build_article_dict(response_dict, url) -> dict:
+    """Builds a dictionary for each article.
 
     Args:
-        submission_dicts (_type_): _description_
-        submission_id (_type_): _description_
         response_dict (_type_): _description_
+        url (str): URL of Hacker News article
     """
     submission_dict = {
             'title': response_dict.get('title', 'None'),
-            'hn_link': f"https://news.ycombinator.com/item?id={submission_id}",
+            'hn_link': url,
             'comments': response_dict.get('descendants', -1),
         }
-    submission_dicts.append(submission_dict)
+    return submission_dict
 
 def print_article_info(submission_dicts):
     for submission_dict in submission_dicts:
@@ -47,13 +44,18 @@ def print_article_info(submission_dicts):
         print(f"Comments: {submission_dict['comments']}")
 
 def main():
-    r = check_status()
+    main_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    r = check_status(main_url)
     submission_ids = r.json()
 
     submission_dicts = []
     for submission_id in submission_ids[0:30]:
-        response_dict = make_api_call(submission_id)
-        build_article_dict(submission_dicts, submission_id, response_dict)
+        sub_url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+        response_dict = make_api_call(sub_url, submission_id)
+
+        hn_link = f"https://news.ycombinator.com/item?id={submission_id}"
+        submission_dict = build_article_dict(response_dict, hn_link)
+        submission_dicts.append(submission_dict)
 
     submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
                             reverse=True)
