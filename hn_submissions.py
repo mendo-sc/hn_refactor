d65a5300c3d241ba47afa8feeec97e6ca9579a50
mendo-sc <mendo@student.cscc.edu>
2025-05-10 16:00:03 -0400

    Edited for type hints and documentation

--- hn_submissions.py
+++ hn_submissions.py
@@ -2,7 +2,7 @@ from operator import itemgetter
 
 import requests
 
-def main_api_call(url) -> requests.Response:
+def main_api_call(url:str) -> requests.Response:
     """Makes an API call to top stories server. Returns response and prints status code.
 
     Args:
@@ -15,7 +15,7 @@ def main_api_call(url) -> requests.Response:
     print(f"Status code: {r.status_code}")
     return r
 
-def submission_api_call(url, submission_id) -> requests.Response:
+def submission_api_call(url:str, submission_id:int) -> requests.Response:
     """Makes a API call to a submission server. Returns response and prints id and status code.
 
     Args:
@@ -30,7 +30,7 @@ def submission_api_call(url, submission_id) -> requests.Response:
     return r
 
     
-def build_article_dict(response_dict, url) -> dict:
+def build_article_dict(response_dict:dict, url:str) -> dict[str,str,int]:
     """Builds a custom dictionary for each article including title, link, and comments.
 
     Args:
@@ -44,7 +44,12 @@ def build_article_dict(response_dict, url) -> dict:
         }
     return submission_dict
 
-def print_article_info(submission_dicts):
+def print_article_info(submission_dicts:list) -> None:
+    """Prints title, discussion link, and comments of each submission dictionary.
+
+    Args:
+        submission_dicts (list): A list of all submission dictionaries
+    """
     for submission_dict in submission_dicts:
         print(f"\nTitle: {submission_dict['title']}")
         print(f"Discussion link: {submission_dict['hn_link']}")
@@ -68,7 +73,7 @@ def main():
     submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
                             reverse=True)
 
-    # print_article_info(submission_dicts)
+    print_article_info(submission_dicts)
 
 if __name__ == '__main__':
     main()
